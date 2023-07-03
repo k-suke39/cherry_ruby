f8004dfb31e532872f0740db1b2cd943c2a6e9bd
k-suke39 <pz.mozarla@gmail.com>
2023-07-03 13:57:17 +0900

    ⚡️

--- chapter2/index.rb
+++ chapter2/index.rb
@@ -1,47 +1,5 @@
-#多重代入
-a,b = 10
+n = 11
 
-puts a
-puts b #代入されない
+p n > 10 ? "10以上" : "10以下"
 
-
-a,b = 10,2
-
-puts a
-puts b #代入される
-
-a,b = 10,2,100
-
-puts a
-puts b #代入される
-
-
-#2つの変数に同じ値を代入する
-a = b = 100
-
-puts a
-puts b 
-
-p 1/2
-p 1.0 /2
-
-
-p 2 ** 3
-
-p n = 1
-p n += 1
-p n -= 1
-
-#式展開する場合は暗黙的に変換される
-
-number = 3
-
-p "number is #{number}"
-
-p %!Bye!
-
-a =<<TEXT
-これはヒアドキュメントです
-TEXT
-
-p a
+p ''.empty?
